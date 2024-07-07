c04c341c235d246619fdc979a0ce65eba8e9214a
Stanley Shen <stanleyshenlg@gmail.com>
2024-07-06 23:57:21 -0700

    update to dram

--- src/dram.v
+++ src/dram.v
@@ -1,21 +1,28 @@
-module dram(CLK, ADDR, DATA, MW, Q);
+module dram(CLK, ADDR, DATA_SEL, DATA_WRITE, DATA_LOW_12BIT, DATA_UPPER_20BIT, DATA_BYTE, MW);
 	input CLK;
-	input [7:0] ADDR;  // 8-bit addresses
-	input [31:0] DATA;  // 32-bit data words
+	input [9:0] ADDR;  // 10-bit addresses
+	input [1:0] DATA_SEL;
+
 	input MW;
+	input [7:0] DATA_WRITE;
   
-	output [31:0] Q;
-	  
-	reg [31:0] Q;
+	output [31:20] DATA_LOW_12BIT;
+	output [19:0] DATA_UPPER_20BIT;
+	output [7:0] DATA_BYTE;
+
+
+	reg DATA_LOW_12BIT;
+	reg DATA_UPPER_20BIT;
+	reg DATA_BYTE;
   
 
-	reg [31:0] mem[0:255];
+	reg [7:0] mem[0:1023];
   
 	integer i;
 
 	initial begin
-		for (i = 0; i < 256; i = i + 1) begin
-			mem[i] = 32'd0;
+		for (i = 0; i < 1024; i = i + 1) begin
+			mem[i] = 8'b00000000;
 		end
 	end
 
@@ -23,13 +30,30 @@ module dram(CLK, ADDR, DATA, MW, Q);
 		// memory is currently initialized with a LUT in 2-byte BCD words
 		//   for the heart rate monitor (big endian)
 
-		if (MW  == 1'b0) begin
-			Q <= mem[ADDR];
+		if (MW  == 1'b0 & DATA_SEL == 2'b00) begin
+			DATA_LOW_12BIT <= {mem[ADDR][7:0], mem[ADDR+1][7:4]};
+			DATA_UPPER_20BIT <= 20'bx;
+			DATA_BYTE <= 8'bx;
 		end
 
-		if (MW  == 1'b1) begin
-			mem[ADDR] <= DATA;
-			Q <= 32'bx;
+		if (MW  == 1'b0 & DATA_SEL == 2'b01) begin
+			DATA_UPPER_20BIT <= 20'bx;
+			DATA_LOW_12BIT <= 12'bx;
+			DATA_BYTE <= 8'bx;
+
+		end
+
+		if (MW  == 1'b0 & DATA_SEL == 2'b10) begin
+			DATA_UPPER_20BIT <= 20'bx;
+			DATA_LOW_12BIT <= 12'bx;
+			DATA_BYTE <= mem[ADDR];
+
+		end
+
+		if (MW == 1'b1) begin
+			mem[ADDR] <= DATA_WRITE;
+			DATA_LOW_12BIT <= 12'bx;
+			DATA_UPPER_20BIT <= 20'bx;
 		end
 	end
 	
--- src/dram_test.v
+++ src/dram_test.v
@@ -3,10 +3,14 @@
 module dram_test();
 
   // for all of your input pins, declare them as type reg, and name them identically to the pins
-  reg [7:0] ADDR;
-  reg [31:0] DATA;
+  reg [9:0] ADDR;
+  reg [1:0] DATA_SEL;
+  reg [7:0] DATA_WRITE;
   reg MW;
-  wire [31:0] Q;
+
+  wire [11:0] DATA_LOW_12BIT;
+  wire [19:0] DATA_UPPER_20BIT;
+  wire [7:0] DATA_BYTE;
 
 
   reg CLK;
@@ -14,9 +18,12 @@ module dram_test();
   dram UUT(
         .CLK(CLK), 
         .ADDR(ADDR), 
-        .DATA(DATA), 
-        .MW(MW), 
-        .Q(Q)
+        .DATA_SEL(DATA_SEL), 
+        .DATA_WRITE(DATA_WRITE),
+        .DATA_LOW_12BIT(DATA_LOW_12BIT), 
+        .DATA_UPPER_20BIT(DATA_UPPER_20BIT),
+        .DATA_BYTE(DATA_BYTE),
+        .MW(MW)
   );
 
   // CLK: generate a 10 MHz clock (rising edge does not start until
@@ -33,31 +40,60 @@ module dram_test();
   
   initial begin
       // it includes input values...
-      ADDR = 8'b0000000;
-      DATA = 32'b1;
+      ADDR = 10'h0;
+      DATA_WRITE = 8'b11111111;
       MW = 1'b1;
+      DATA_SEL = 2'bx;
 
       #100;  // wait for input signals to propagate through circuit
-      $display("Result is = %b", Q);
 
-      ADDR = 8'b0000000;
-      MW = 1'b0;
-
-      #100;  // wait for input signals to propagate through circuit
-      $display("Result is = %b", Q);
-
-      ADDR = 8'd255;
-      DATA = 32'd21;
+      ADDR = 10'h1;
+      DATA_WRITE = 8'b00000000;
       MW = 1'b1;
+      DATA_SEL = 2'bx;
 
       #100;  // wait for input signals to propagate through circuit
-      $display("Result is = %d", Q);
 
-      ADDR = 8'd255;
+      ADDR = 10'h2;
+      DATA_WRITE = 8'b11111111;
+      MW = 1'b1;
+      DATA_SEL = 2'bx;
+
+      #100;  // wait for input signals to propagate through circuit
+
+      ADDR = 10'h3;
+      DATA_WRITE = 8'b00000000;
+      MW = 1'b1;
+      DATA_SEL = 2'bx;
+
+      #100;  // wait for input signals to propagate through circuit
+
+      ADDR = 10'h0;
+      DATA_WRITE = 8'bx;
       MW = 1'b0;
+      DATA_SEL = 2'b00;
 
-      #100;  // wait for input signals to propagate through circuit
-      $display("Result is = %d", Q);
+      #100;
+      $display("Result is = %b", DATA_LOW_12BIT);
+
+      ADDR = 10'h0;
+      DATA_WRITE = 8'bx;
+      MW = 1'b0;
+      DATA_SEL = 2'b01;
+
+      #100;
+      $display("Result is = %b", DATA_UPPER_20BIT);
+
+      ADDR = 10'h0;
+      DATA_WRITE = 8'bx;
+      MW = 1'b0;
+      DATA_SEL = 2'b10;
+
+      #100;
+      $display("Result is = %b", DATA_BYTE);
+
+
+      $finish; // need this to send sim
       
     end
 endmodule
