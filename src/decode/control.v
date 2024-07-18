0af085189a71a0418ee88cc0553313cdebe62b00
Stanley Shen <stanleyshenlg@gmail.com>
2024-07-17 23:00:43 -0700

    control update

--- src/decode/control.v
+++ src/decode/control.v
@@ -81,7 +81,7 @@ module control(CLK, OPCODE, FUNCT_FIVE, FUNCT_THREE, FC, IMM_MUX, WREG, WMEM, RM
                     end
                 endcase
             end
-            7'b1100011: begin // normal logical operations with 
+            7'b1100011: begin // branching operation + JAL + JALR 
                 case (FUNCT_THREE)
                     3'b000: begin
                         FC = 1'b001;
@@ -125,6 +125,185 @@ module control(CLK, OPCODE, FUNCT_FIVE, FUNCT_THREE, FC, IMM_MUX, WREG, WMEM, RM
                 IMM_MUX = 1'0;
                 BRANCH = 1'b1;
             end
+
+            7'b0110111: begin // Immediate Logical operations 
+                case (FUNCT_THREE)  
+                    3'b000: begin // ADDI 
+                        FC = 3'b000;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b010: begin // SLTI 
+                        FC = 3'b000;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b100: begin // XORI  
+                        FC = 3'b100; // I think this is the right FC code?
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b100: begin // ORI   
+                        FC = 3'b011; // I think this is the right FC code?
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b100: begin // ANDI    
+                        FC = 3'b010; // I think this is the right FC code?
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b100: begin // SLLI    
+                        FC = 3'b101; // I think this is the right FC code?
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b1;
+                        BRANCH = 1'b0;
+                    end
+                    3'b101: begin // SRLI and SRAI 
+                        case (FUNCT_FIVE)  
+                            5'b00000 : begin // SRLI 
+                                FC = 3'b110; // I think this is the right FC code?
+                                WREG = 1'b1;
+                                WMEM = 1'b0;
+                                RMEM = 1'b0;
+                                IMM_MUX = 1'b1;
+                                BRANCH = 1'b0;
+                            end 
+                            5'b01000: begin   // SRAI    
+                                FC = 3'b111; // I think this is the right FC code?
+                                WREG = 1'b1;
+                                WMEM = 1'b0;
+                                RMEM = 1'b0;
+                                IMM_MUX = 1'b1;
+                                BRANCH = 1'b0;
+                            end 
+                        endcase 
+                    end
+        
+                    default: begin
+                        FC = 3'bxxx;
+                        WREG = 1'b0;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+                endcase
+            end
+
+            7'b0000011: begin // Load OPs
+                case (FUNCT_THREE)  
+                    3'b000: begin // LB 
+                        FC = 3'b000;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b1;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    3'b001: begin // LH 
+                        FC = 3'b001;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b1;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    3'b010: begin // LW 
+                        FC = 3'b010;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b1;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    3'b100: begin // LBU 
+                        FC = 3'b100;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b1;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    3'b101: begin // LHU 
+                        FC = 3'b101;
+                        WREG = 1'b1;
+                        WMEM = 1'b0;
+                        RMEM = 1'b1;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    default: begin
+                        FC = 3'bxxx;
+                        WREG = 1'b0;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                endcase
+            end
+
+            7'b0100011:  // Store OPs
+                    3'b000: begin // SB 
+                        FC = 3'b101;
+                        WREG = 1'b0;
+                        WMEM = 1'b1;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+                    3'b001: begin // SH 
+                        FC = 3'b101;
+                        WREG = 1'b0;
+                        WMEM = 1'b1;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+                    3'b010: begin // SB 
+                        FC = 3'b101;
+                        WREG = 1'b0;
+                        WMEM = 1'b1;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+
+                    default: begin
+                        FC = 3'bxxx;
+                        WREG = 1'b0;
+                        WMEM = 1'b0;
+                        RMEM = 1'b0;
+                        IMM_MUX = 1'b0;
+                        BRANCH = 1'b0;
+                    end
+                endcase
+            end
+
         endcase
     end
 
